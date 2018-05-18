74706ec549c17431fbc5567450965b5a94014972
Chatura Ahangama <chaturadroid@gmail.com>
2018-05-18 13:44:03 -0700

    JS

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+var authKey = '10dd49c028ae47bab105dff84789127c';
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+  authKey + "&q=";
+
+
+function makeQuery()
+{
+   var searchTerm = $("").text();
+   var recordNum = $("").text();
+   var year = $("").text();
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) 
+  
+  {
+
+
+
+  });
+
+};
