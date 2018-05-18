798fc2a128c616d6b5a2d0857c3a6faee93a48ca
Chatura Ahangama <chaturadroid@gmail.com>
2018-05-18 14:56:16 -0700

    backend Update

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,39 @@
 var authKey = '10dd49c028ae47bab105dff84789127c';
-var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
-  authKey;
 
 
 function makeQuery()
 {
+  var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json" ;
+
    var searchTerm = $("#search-term").val().trim();
    var recordNum = $("#search-records").val().trim();
    var yearStart = $("#start-year").val().trim();
    var yearEnd = $("#end-year").val().trim();
 
-   queryURLBase += "?" + $.param({ 'q': searchTerm,
+   queryURLBase += '?' + $.param({
+    'api-key': authKey,
+    'q': searchTerm,
     'begin_date': yearStart+"0101",
-    'end_date': yearEnd+"01001"
+    'end_date': yearEnd+"0101"
   });
-$.ajax({
+
+  $.ajax({
     url: queryURLBase ,
     method: "GET"
-  }).then(function(response)  
+  }).then(function(event)  
   {
-
-    console.log(response);
-
+    console.log(event);
+    var articles = event.response.docs;
+    $("#article-row").empty();
+    for(var ii=0;ii<recordNum;ii++)
+    {
+        var articleLink= articles[ii].web_url;
+        var header=articles[ii].snippet;
+        var headLine = $("<a>");
+        headLine.attr("href",articleLink);
+        headLine.text(header);
+        $("#article-row").append(headLine);
+    }
   });
 
 };
